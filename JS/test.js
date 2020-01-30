42cd70da5bfa1d689be2a8ccde066c9edb852211
talintrip <talintrip@gmail.com>
2020-01-30 12:21:31 +0200

    First Commit

---
+++ JS/test.js
@@ -0,0 +1,12 @@
+alert("I am an alert box!");
+
+/*
+const testFolder = './content/';
+const fs = require('fs');
+
+fs.readdirSync(testFolder).forEach(file => {
+    console.log(file);
+});
+}
+
+*/
